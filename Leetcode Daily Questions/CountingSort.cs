fb14754e5f94cf221c0b183ee3b2bcef89f03946
Saurabh Godse <saurabh.godse@bp.com>
2024-07-25 13:16:43 +0530

    counting sort

---
+++ Leetcode Daily Questions/CountingSort.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Leetcode_Daily_Questions
+{
+    public class CountingSort
+    {
+        public static void Main(string[] args)
+        {
+            CountingSort countingSort = new CountingSort();
+            int[] nums = { 5, 2, 3, 1 };
+            countingSort.SortArray(nums);
+        }
+
+        /*
+         * Problem 912 Sort an array can also be solved using counting sort. This is giving best time complexity for that problem.
+         */
+        public int[] SortArray(int[] nums)
+        {
+            int min = nums.Min();
+            int max = nums.Max();
+            int[] freq = new int[max - min + 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                freq[nums[i] - min]++;
+            }
+            int j = 0;
+            for (int i = 0; i < freq.Length; i++)
+            {
+                if (freq[i] > 0)
+                {
+                    while (freq[i] > 0)
+                    {
+                        freq[i]--;
+                        nums[j++] = i + min;
+                    }
+                }
+                if (j >= nums.Length)
+                {
+                    break;
+                }
+            }
+            return nums;
+        }
+    }
+}
