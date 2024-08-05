8c1e4a38fde6d0311d7688d8ba43a58aa07afce6
Saurabh Godse <saurabh.godse@bp.com>
2024-08-05 12:31:10 +0530

    2053. Kth Distinct String in an Array

--- Python Solutions/KthDistinctStringInArray.py
+++ Python Solutions/KthDistinctStringInArray.py
@@ -1,1 +1,21 @@
-class KthDistinctStringInArray
+"""
+2053. Kth Distinct String in an Array
+A distinct string is a string that is present only once in an array.
+
+Given an array of strings arr, and an integer k, return the kth distinct string present in arr.
+If there are fewer than k distinct strings, return an empty string "".
+
+Note that the strings are considered in the order in which they appear in the array.
+"""
+
+class KthDistinctStringInArray:
+	def kthDistinct(self, arr: List[str], k: int) -> str:
+        count = defaultdict(int)
+        for s in arr:
+            count[s] += 1
+        for s in arr:
+            if count[s] == 1:
+                k -= 1
+            if k == 0:
+                return s
+        return ""
